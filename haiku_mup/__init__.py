3913adb8c6ac0e4eb47615746b1fe4240ac3931d
Davis <davis.yoshida@gmail.com>
2022-04-27 15:39:15 -0500

    Add shared layers to __init__.py

--- haiku_mup/__init__.py
+++ haiku_mup/__init__.py
@@ -1,6 +1,6 @@
 """"A simple port of μP  to Haiku/JAX."""
 
 from .mup import get_shapes, Mup, apply_mup
-from .module import Readout
+from .module import Readout, SharedEmbed, SharedReadout
 
 __version__ = "0.1.1"
